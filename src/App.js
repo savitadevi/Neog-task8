8f85ac60b8702cc958cc1583568868cd4cda19e7
savitadevi <savitadevi20@navgurukul.org>
2021-12-07 23:31:56 +0530

    showbooks

--- src/App.js
+++ src/App.js
@@ -1,25 +1,86 @@
-import logo from './logo.svg';
-import './App.css';
+// import './App.css';
 
-function App() {
+// function App() {
+//   return (
+//     <div className="App">
+       
+//           Learn React
+      
+//     </div>
+//   );
+// }
+
+// export default App;
+
+
+
+import React from "react";
+import "./styles.css";
+import { useState } from "react";
+var books = {
+  JavaScript: [
+    {
+      name: "Javascript The Good Parts",
+      rating: "4/5"
+    },
+    {
+      name: "Clean Code in JavaScript ",
+      rating: "5/5"
+    }
+  ],
+
+  fiction: [
+    {
+      name: "The Girl Who Drank the Moon ",
+      rating: "5/5"
+    },
+    {
+      name: "Ghosts of The Silent Hills: Stories based on true hauntings",
+      rating: "4/5"
+    }
+  ],
+  business: [
+    {
+      name: "Before You Start Up",
+      rating: "4/5"
+    },
+    {
+      name: "The Tatas",
+      rating: "5/5"
+    }
+  ]
+};
+var BooksWehave = Object.keys(books);
+
+export default function App() {
+  var [generSelect, setGener] = useState("business");
+  function genreClickHandler(book) {
+    setGener(book);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>📚 goodBooks</h1>
+      <p>Checkout my favorite books. Select a genre to get started</p>
+      <div>
+        {BooksWehave.map(function (book) {
+          return (
+            <button onClick={() => genreClickHandler(book)}>{book}</button>
+          );
+        })}
+      </div>
+      <hr />
+      <div style={{ textAlign: "left" }}></div>
+      <ul>
+        {books[generSelect].map(function (bookName) {
+          return (
+            <li>
+              <div>{bookName.name}</div>
+              <div>{bookName.rating}</div>
+            </li>
+          );
+        })}
+      </ul>
     </div>
   );
 }
-
-export default App;
